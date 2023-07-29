7a369281775e78e99504a316661839c979ad7ef4
MatthieuGallice <frooztor@gmail.com>
2023-07-29 16:08:37 +0200

    Modification du noms des routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,11 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/ordering', 'OrderingsController.index') //getall
-Route.post('/ordering', 'OrderingsController.store') //create
-Route.get('/ordering/:id', 'OrderingsController.show') //getbyid
-Route.patch('/ordering/:id', 'OrderingsController.update') //update
+Route.get('/orders', 'OrdersController.index') //getall
+Route.post('/orders', 'OrdersController.store') //create
+Route.get('/orders/:id', 'OrdersController.show') //getbyid
+Route.patch('/orders/:id', 'OrdersController.update') //update
 
-Route.get('/providerOrders', 'ProvidersOrdersController.index') //getall
-Route.post('/providerOrders', 'ProvidersOrdersController.store') //create
-Route.get('/providerOrders/:id', 'ProvidersOrdersController.show') //getbyid
-Route.patch('/providerOrders/:id', 'ProvidersOrdersController.update') //update
+Route.get('/providers/orders', 'ProvidersOrdersController.index') //getall
+Route.post('/providers/orders', 'ProvidersOrdersController.store') //create
+Route.get('/providers/orders/:id', 'ProvidersOrdersController.show') //getbyid
+Route.patch('/providers/orders/:id', 'ProvidersOrdersController.update') //update
